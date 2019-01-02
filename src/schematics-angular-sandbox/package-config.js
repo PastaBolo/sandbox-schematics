e68edc465e9290c9c3698a473acbdb81744d0456
Alain P <alain.paolini291@gmail.com>
2019-01-02 23:14:27 +0100

    initial commit

--- src/schematics-angular-sandbox/package-config.js
+++ src/schematics-angular-sandbox/package-config.js
@@ -1,15 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const scripts = {
-    build: 'tsc -p tsconfig.json',
-    clean: 'git checkout HEAD -- sandbox && git clean -f -d sandbox',
-    launch: 'cd sandbox && ng g schematic-with-sandbox:schematic-with-sandbox',
-    'clean:launch': 'npm run clean && npm run launch',
-    'build:clean:launch': 'npm run build && npm run clean && npm run launch',
-    'link:schematic': 'npm link && cd sandbox && npm link schematic-with-sandbox'
-};
 function updatePackageJsonScripts() {
     return (tree, _context) => {
+        const schematicName = _context.schematic.description.name;
+        const scripts = {
+            build: 'tsc -p tsconfig.json',
+            clean: 'git checkout HEAD -- sandbox && git clean -f -d sandbox',
+            launch: `cd sandbox && ng g ${schematicName}:${schematicName}`,
+            'clean:launch': 'npm run clean && npm run launch',
+            'build:clean:launch': 'npm run build && npm run clean && npm run launch',
+            'link:schematics': `npm link && cd sandbox && npm link ${schematicName}`
+        };
         if (tree.exists('package.json')) {
             const json = JSON.parse(tree.read('package.json').toString('utf-8'));
             if (!json.scripts)
--- src/schematics-angular-sandbox/package-config.ts
+++ src/schematics-angular-sandbox/package-config.ts
@@ -1,16 +1,16 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics'
 
-const scripts = {
-  build: 'tsc -p tsconfig.json',
-  clean: 'git checkout HEAD -- sandbox && git clean -f -d sandbox',
-  launch: 'cd sandbox && ng g schematic-with-sandbox:schematic-with-sandbox',
-  'clean:launch': 'npm run clean && npm run launch',
-  'build:clean:launch': 'npm run build && npm run clean && npm run launch',
-  'link:schematic': 'npm link && cd sandbox && npm link schematic-with-sandbox'
-}
-
 export function updatePackageJsonScripts(): Rule {
   return (tree: Tree, _context: SchematicContext): Tree => {
+    const schematicName = _context.schematic.description.name
+    const scripts = {
+      build: 'tsc -p tsconfig.json',
+      clean: 'git checkout HEAD -- sandbox && git clean -f -d sandbox',
+      launch: `cd sandbox && ng g ${schematicName}:${schematicName}`,
+      'clean:launch': 'npm run clean && npm run launch',
+      'build:clean:launch': 'npm run build && npm run clean && npm run launch',
+      'link:schematics': `npm link && cd sandbox && npm link ${schematicName}`
+    }
     if (tree.exists('package.json')) {
       const json = JSON.parse(tree.read('package.json')!.toString('utf-8'))
       if (!json.scripts) json.scripts = {}
