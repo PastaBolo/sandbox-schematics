6f988161b386df75766586596db4446aca8e8bcb
PastaBolo <alain.paolini29>
2019-01-02 16:52:36 +0100

    initial commit

--- src/schematics-angular-sandbox/index.ts
+++ src/schematics-angular-sandbox/index.ts
@@ -1,10 +1,28 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, chain, externalSchematic } from '@angular-devkit/schematics'
+import { Schema as NgNewOptions } from '@schematics/angular/ng-new/schema'
 
+const opt: NgNewOptions = { name: 'sandbox', version: '1', skipInstall: true }
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function schematicsAngularSandbox(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
+const scripts = {
+  build: 'tsc -p tsconfig.json',
+  clean: 'git checkout HEAD -- sandbox && git clean -f -d sandbox',
+  launch: 'cd sandbox && ng g schematic-with-sandbox:schematic-with-sandbox',
+  'clean:launch': 'npm run clean && npm run launch',
+  'build:clean:launch': 'npm run build && npm run clean && npm run launch',
+  'link:schematic': 'npm link && cd sandbox && npm link schematic-with-sandbox'
+}
+
+export default function(_options: any): Rule {
+  return chain([
+    (tree: Tree, _context: SchematicContext): Tree => {
+      if (tree.exists('package.json')) {
+        const json = JSON.parse(tree.read('package.json')!.toString('utf-8'))
+        if (!json.scripts) json.scripts = {}
+        Object.assign(json.scripts, scripts)
+        tree.overwrite('package.json', JSON.stringify(json, null, 2))
+      }
+      return tree
+    },
+    externalSchematic('@schematics/angular', 'ng-new', opt)
+  ])
 }
