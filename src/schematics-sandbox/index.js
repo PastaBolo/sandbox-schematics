f87e4397c697c187b832792f9a423ca486153ab2
PastaBolo <alain.paolini29>
2019-01-03 15:22:38 +0100

    initial commit

--- src/schematics-sandbox/index.js
+++ src/schematics-sandbox/index.js
@@ -4,7 +4,7 @@ const schematics_1 = require("@angular-devkit/schematics");
 const package_config_1 = require("../utility/package-config");
 function default_1(options) {
     options.name = options.name || 'sandbox';
-    const ngNewOptions = { name: options.name, version: '1', skipInstall: true };
+    const ngNewOptions = { name: options.name, version: '1', skipInstall: true, skipGit: true };
     return schematics_1.chain([package_config_1.updatePackageJsonScripts(options), schematics_1.externalSchematic('@schematics/angular', 'ng-new', ngNewOptions)]);
 }
 exports.default = default_1;
